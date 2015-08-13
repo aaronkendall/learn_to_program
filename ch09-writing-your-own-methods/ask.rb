bd4f821483744fdaddafecbde8121e470b4f3fdf
Aaron <aarongkendall@gmail.com>
2015-08-13 10:13:20 +0100

    ask solved

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -1,3 +1,17 @@
-def ask question
-  # your code here
+def ask(question)
+	puts question
+	answer = gets.chomp.downcase
+
+  	if answer == "yes"
+  		return true
+  	elsif answer == "no"
+  		return false
+  	end
+  	
+  
+  	puts "Please answer 'yes' or 'no'."
+  reply
 end
+
+ask("Do you live in England?")
+ask("Do you like Ruby?")
