00082918498db90374089fc39b8dad0b181f21fc
Aaron <aarongkendall@gmail.com>
2015-08-13 11:49:47 +0100

    roman numerals solved

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,18 @@
-def roman_numeral num
-  # your code here
+def roman_numeral(num)
+  new_roman = ""
+
+  new_roman = new_roman + "M" * (num / 1000)
+  new_roman = new_roman + "D" * (num % 1000 / 500)
+  new_roman = new_roman + "C" * (num % 500 / 100)
+  new_roman = new_roman + "L" * (num % 100 / 50)
+  new_roman = new_roman + "X" * (num % 50 / 10)
+  new_roman = new_roman + "V" * (num % 10 / 5)
+  new_roman = new_roman + "I" * (num % 5 / 1)
+
+  new_roman.gsub! "VIIII", "IX"
+  new_roman.gsub! "IIII", "IV"
+  new_roman
+
 end
+
+puts roman_numeral(59)
